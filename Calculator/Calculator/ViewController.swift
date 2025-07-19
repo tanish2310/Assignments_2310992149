10ddd09fa9c8c87029d038ddbb1117be4647a2f6
Tanish Wadhwa <tanish2149.be23@chitkara.edu.in>
2025-07-19 15:58:59 +0530

    initial Commit

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Tanish on 17/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var OutputLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
+
